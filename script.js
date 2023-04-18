9d411de9ef3c6f23c4301dede8f74cd498b3d5a6
Jebder <zdbrown2343@gmail.com>
2023-04-18 01:41:17 -0400

    Initial commit

--- script.js
+++ script.js
@@ -31,31 +31,26 @@ function playRound(playerSelection, computerSelection){
     return result;
 };
 
+function displayScore(pScore, cScore) {
+    const score = document.querySelector('#score');
+    score.textContent = `${pScore} : ${cScore}`;
+};
+
+function updateRound(roundNum) {
+    const score = document.querySelector('h2');
+    score.textContent = `Round ${roundNum}!`
+};
+
 function game(){
+
+    let playerSelection = '';
     let pScore = 0;
     let cScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let computerSelection = getComputerChoice();
-        let playerSelection = prompt('Choose your attack!');
-        let result = playRound(playerSelection, computerSelection)
-        if (result == 'You Win!') {
-            pScore = pScore + 1;
-        }
-        if (result == 'You Lose!'){
-            cScore = cScore + 1;
-        }
-        console.log(result)
-        console.log(`Player score = ${pScore}`)
-        console.log(`Computer score = ${cScore}`)
-        
-    }
-    if (pScore > cScore){
-        console.log('Player Wins!')
-    }
-    else {
-        console.log('Machine wins')
+    const buttons = document.querySelectorAll('.button');
+    buttons.addEventListener('click', () => { })
+    for (let i = 0; i < 5; i++){
+
     }
 };
 
-    
-
+game();
